99f74d9f46d1b25432f0120ba9795c894fa7d021
James Brundage <james.brundage@start-automating.com>
2021-03-31 12:53:44 -0700

    Checking in Azure DevOps Pipeline

---
+++ ScriptDeck.ADOPipeline.PSDevOps.ps1
@@ -0,0 +1,6 @@
+﻿#requires -Module PSDevOps
+Push-Location $PSScriptRoot
+New-ADOPipeline -Stage PowerShellStaticAnalysis, TestPowerShellCrossPlatform, UpdatePowerShellGallery |
+    Set-Content .\ScriptDeck.azure-pipeline.yml -Encoding UTF8
+Pop-Location
+
