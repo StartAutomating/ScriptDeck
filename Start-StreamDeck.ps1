432c104debf5f9f8fad2ca2501d078e15de0475a
James Brundage <james.brundage@start-automating.com>
2021-09-23 13:06:46 -0700

    Adding Start/Stop/Restart-StreamDeck

---
+++ Start-StreamDeck.ps1
@@ -0,0 +1,45 @@
+﻿function Start-StreamDeck
+{
+    <#
+    .Synopsis
+        Starts StreamDeck
+    .Description
+        Starts the StreamDeck application.
+    .Example
+        Start-StreamDeck
+    .Link
+        Restart-StreamDeck
+    #>
+    param(
+    # The Path to StreamDeck.  
+    # If this parameter is provided, this will attempt to launch StreamDeck from the default installation folder.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $StreamDeckPath,
+
+    # If set, will output the created process.
+    [switch]
+    $PassThru
+    )
+
+    process {
+        if (-not $StreamDeckPath) {
+            $streamDeckProcess = Get-Process StreamDeck -ErrorAction SilentlyContinue
+            $StreamDeckPath =
+                if ($streamDeckProcess) {
+                    $streamDeckProcess.Path
+                } elseif ((-not $PSVersionTable.Platform) -or ($PSVersionTable.Platform -match 'Win')) {
+                    "$env:ProgramFiles\Elgato\StreamDeck\StreamDeck.exe"
+                } elseif ($PSVersionTable.Platform -eq 'Unix' -and $PSVersionTable.OS -like '*darwin*') {
+                    "/Applications/Stream Deck.app”
+                }
+        }
+
+        if (-not $StreamDeckPath) {
+            Write-Error "Could not automatically detect a -StreamDeckPath.  Please provide one"
+            return
+        } else {
+            Start-Process $StreamDeckPath -PassThru:$PassThru
+        }
+    }
+}
---
+++ Stop-StreamDeck.ps1
@@ -0,0 +1,42 @@
+﻿function Stop-StreamDeck
+{
+    <#
+    .Synopsis
+        Stops the StreamDeck application
+    .Description
+        Stops the StreamDeck application.  Attempts to gracefully exit plugins.
+    .Example
+        Stop-StreamDeck
+    #>
+    [OutputType([Diagnostics.Process])]
+    param(
+    # If set, will output the process
+    [switch]
+    $PassThru
+    )
+    
+    $streamdeckprocess = Get-Process streamdeck -ErrorAction SilentlyContinue
+    if (-not $streamdeckprocess) { 
+        Start-StreamDeck -PassThru:$PassThru
+        return 
+    }
+    $streamDeckPath    = "$($streamdeckprocess.Path)"
+    if ($streamDeckPath) { 
+        Start-Process -FilePath $streamDeckPath -ArgumentList '--quit' -Wait -PassThru:$PassThru
+    }
+        
+    $streamdeckprocess = Get-Process streamdeck -ErrorAction SilentlyContinue
+    Register-ObjectEvent -InputObject $streamdeckProcess -EventName Exited -Action ([ScriptBlock]::Create(@"
+Write-Verbose 'StreamDeck Process Exited'
+New-Event -SourceIdentifier StreamDeck.Stopped -MessageData ($(if ($PassThru) { "`$event.Sender"}))
+"@)) |Out-Null
+
+    Wait-Event -SourceIdentifier StreamDeck.Stopped | 
+        ForEach-Object {
+            if ($PassThru) {
+                $_.MessageData
+            }
+        }
+    
+}
+
