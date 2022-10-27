a406a45769b9b8680f87034e19503e53e5b49c34
James Brundage <@github.com>
2022-10-27 16:11:30 -0700

    Adding Start-PowerShell (Fixes #41)

---
+++ com.start-automating.scriptdeck.sdPlugin/On_com.start-automating.scriptdeck.Start-PowerShell.KeyDown.ps1
@@ -0,0 +1,37 @@
+﻿$powerShellArgs = @(
+if ($event.MessageData.payload.settings.NoLogo) {
+    "-nologo"
+}
+if ($event.MessageData.payload.settings.NoProfile) {
+    "-noprofile"
+}
+if ($event.MessageData.payload.settings.NoExit) {
+    "-noexit"
+}
+if ($event.MessageData.payload.settings.WorkingDirectory) {
+    "-workingdirectory"
+    $event.MessageData.payload.settings.WorkingDirectory
+}
+
+if ($event.MessageData.payload.settings.ScriptFile) {
+    "-file"
+    $scriptfile = 
+        $event.MessageData.payload.settings.ScriptFile
+    if ($scriptfile -like 'file:///*') {
+        ([uri]$scriptfile).AbsolutePath
+    } else {
+        [Web.HttpUtility]::UrlDecode(
+            ($event.MessageData.payload.settings.ScriptFile -replace '^C\:\\fakepath\\')) -replace 
+                '[\\/]', ([IO.Path]::DirectorySeparatorChar)
+    }
+}
+elseif ($event.MessageData.payload.settings.StartupScript) {
+    "-encodedcommand"    
+    [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($event.MessageData.payload.settings.StartupScript))
+}
+
+)
+
+$exePath = (Get-Process -id $pid).Path
+Start-Process -FilePath $exePath -ArgumentList $powerShellArgs
+
