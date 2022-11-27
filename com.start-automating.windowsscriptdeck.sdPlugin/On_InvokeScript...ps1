c08e626d4cc783756d752638d79ab0926bd4862d
James Brundage <@github.com>
2022-11-26 19:03:06 -0800

    WindowsScriptDeck:  Simplifying InvokeScript implementation

---
+++ com.start-automating.windowsscriptdeck.sdPlugin/On_InvokeScript...ps1
@@ -0,0 +1,17 @@
+# This file handles all InvokeScript related events
+
+$invokeError = $null
+
+foreach ($settingName in 'KeyDown','KeyUp','WillAppear', 'WillDisappear') {
+    if ($event.SourceIdentifier -match $settingName -and
+    $event.MessageData.payload.settings.$settingName) {
+        $settingScript = $event.MessageData.payload.settings.$settingName
+        Invoke-Expression $settingScript -ErrorAction Continue -ErrorVariable $invokeError
+        if ($invokeError) {
+            $invokeError | Out-string | Add-Content -Path $global:STREAMDECK_PLUGINLOGPATH
+            Send-StreamDeck -ShowAlert -Context $event.MessageData.Context
+        } else {
+            Send-StreamDeck -ShowOK -Context $event.MessageData.Context
+        }
+    }
+}
