dd0af98e5ff0ecdf14740d98f0b636d43284202e
James Brundage <@github.com>
2022-11-29 15:14:27 -0800

    Get-LoupeDeckProfile:  Adding -Name and -ProfileRoot (re #53)

--- Functions/LoupeDeck/Get-LoupeDeckProfile.ps1
+++ Functions/LoupeDeck/Get-LoupeDeckProfile.ps1
@@ -8,37 +8,52 @@ function Get-LoupeDeckProfile
     .EXAMPLE
         Get-LoupeDeckProfile
     #>
-    param()
+    param(
+    # The name of the profile
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Name,
 
-    process {
-        $loupeDeckProfileFiles = 
-            if ($IsWindows -or (-not $IsMacOS -and -not $IsLinux)) {
-                $env:APPDATA | 
-                    Split-Path | 
-                    Join-Path -ChildPath Local |
-                    Join-Path -ChildPath Loupedeck |
-                    Join-Path -ChildPath Applications |
-                    Get-ChildItem |
-                    Get-ChildItem |
-                    Get-ChildItem -Filter Profiles |
-                    Get-ChildItem |
-                    Get-ChildItem -Filter ProfileInfo.json
-            } elseif ($IsMacOS) {
-                Get-ChildItem "$env:HOME/.local/share/Loupedeck/Applications" |
-                    Get-ChildItem |
-                    Get-ChildItem |
-                    Get-ChildItem -Filter Profiles |
-                    Get-ChildItem |
-                    Get-ChildItem -Filter ProfileInfo.json
-            }
-        foreach ($profileFile in $loupeDeckProfileFiles) {
-            [IO.File]::ReadAllText($profileFile.FullName) |
+    # The profile root.
+    # This will be automatically set if it is not provided.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string[]]
+    $ProfileRoot
+    )
+
+    begin {
+        filter ImportLoupeDeckProfile {
+            [IO.File]::ReadAllText($_.FullName) |
                 ConvertFrom-Json |
                 & { process {
+                    if ($name -and $_.DisplayName -notlike $name) { return } 
                     $_.pstypenames.clear()
                     $_.pstypenames.add('LoupeDeck.Profile')
                     $_
                 } }
         }
+    }
+
+    process {
+        if (-not $ProfileRoot) {
+            $ProfileRoot = 
+                if ($IsWindows -or (-not $IsMacOS -and -not $IsLinux)) {
+                    $env:APPDATA | 
+                        Split-Path | 
+                        Join-Path -ChildPath Local |
+                        Join-Path -ChildPath Loupedeck |
+                        Join-Path -ChildPath Applications
+                } elseif ($IsMacOS) {
+                    Join-Path "$env:HOME/.local/share/Loupedeck" "Applications"
+                }
+        }        
+        if (-not $ProfileRoot) { return }
+        $ProfileRoot |
+            Get-ChildItem |
+            Get-ChildItem |
+            Get-ChildItem -Filter Profiles |
+            Get-ChildItem |
+            Get-ChildItem -Filter ProfileInfo.json |
+            ImportLoupeDeckProfile
     }
 }
