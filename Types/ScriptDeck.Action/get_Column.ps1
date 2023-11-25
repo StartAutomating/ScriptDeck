1e9ace0b1268a8995195c8321d2cf2e922c428ea
James Brundage <@github.com>
2023-11-25 14:32:06 -0800

    feat: ScriptDeck.Action.get Column/Row ( Fixes #89, Fixes #96 )

---
+++ Types/ScriptDeck.Action/get_Column.ps1
@@ -0,0 +1,7 @@
+<#
+.SYNOPSIS
+    Gets an Action's Column
+.DESCRIPTION
+    Gets the column of a StreamDeck action
+#>
+$this.payload.coordinates.column
---
+++ Types/ScriptDeck.Action/get_Row.ps1
@@ -0,0 +1,7 @@
+<#
+.SYNOPSIS
+    Gets an Action's Row
+.DESCRIPTION
+    Gets the row of a StreamDeck action
+#>
+$this.payload.coordinates.row
